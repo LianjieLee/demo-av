2e3255ff44ed823ab519948558146cb67f5829d2
ghcdn <sht-dl@protonmail.com>
2022-06-25 23:40:34 +0800

    simplify

--- uploader.py
+++ uploader.py
@@ -16,9 +16,9 @@ if __name__ == '__main__':
     api_id = os.getenv('API_ID')
     api_hash = os.getenv('API_HASH')
     peer_name = sys.argv[1]
-    video_list = sys.argv[2].split()
-    caption = sys.argv[3]
+    caption = sys.argv[2]
+    video_list = glob("*.mp4")
     video_list.sort()
-    video_list.insert(0, "poster.jpg")
+    video_list.insert(0, "info/poster.jpg")
     print(f"sending {video_list}", flush=True)
     send_video(api_id, api_hash, peer_name, video_list, caption)
